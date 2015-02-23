9e4e4b7a08fb6643adb9b10a123e31a7c12bd426
Mimerme <androssdragonq.9@gmail.com>
2015-02-22 20:16:58 -0500

    Added support for nicknames and timestamps

--- index.js
+++ index.js
@@ -2,21 +2,36 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var connectedUsers = [];
+
+//Socket events
+//server message - message broadcasted to all users from the server
+//user join - when the user specifies a nickname and joins the chat
+//chat message - string broadcasted to all connected users
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+  console.log('A user connected from ip: ' + socket.handshake.address);
   socket.on('disconnect', function(){
-    console.log('user disconnected');
+    console.log('a user disconnected');
   });
-  socket.on('chat message', function(msg){
+  socket.on('chat message', function(msg, nick){
     console.log('message: ' + msg);
-    io.emit('chat message', msg);
+    io.emit('chat message', msg, nick, getTimestamp());
+  });
+  socket.on('user join', function(nick){
+  	connectedUsers.push(nick.toString());
+  	  io.emit('server message', 'User ' + nick + ' has joined');
   });
 });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
+
+function getTimestamp(){
+	return new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '') 
+}
